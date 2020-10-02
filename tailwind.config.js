9f1e56665b55b30de2793895c53687644776cdf0
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-10-02 18:35:21 -0500

    Add assets, font import, and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,20 @@ module.exports = {
   },
   purge: [],
   theme: {
+    fontFamily: {
+      sans:
+        '"DM Sans", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+    },
     extend: {
       colors: {
         ocean: '#072540',
+        pond: '#93C2DB',
+        lake: '#183D5D',
+        fuschia: '#FF8AE2',
+        grayed: 'rgba(198,198,198,0.6)',
+      },
+      minWidth: {
+        '1/2': '50%',
       },
     },
   },
