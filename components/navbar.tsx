9b6691fcb810f6bff92acb6b00f183e3489c8559
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-10-03 02:17:52 -0500

    Increase z-index of navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => (
   <div
     id='navbar'
-    className='py-6 w-full flex items-center justify-center fixed bg-ocean bg-opacity-75 top-0'>
+    className='py-6 w-full flex items-center justify-center fixed bg-ocean bg-opacity-75 top-0 z-50'>
     <div className='min-w-1/2 flex items-center justify-between px-8'>
       <a className='link font-medium text-xl text-white'>Home</a>
       <a className='link font-medium text-xl text-grayed'>Experience</a>
