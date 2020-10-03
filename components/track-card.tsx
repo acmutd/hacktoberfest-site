89c82eaa3231cc96febee5613bc32b75fc9a7410
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-10-03 02:17:30 -0500

    Update TrackCard to toggle visibility of children

--- components/track-card.tsx
+++ components/track-card.tsx
@@ -1,10 +1,34 @@
-const TrackCard = ({ name, description }: { name: string; description: string }): JSX.Element => (
-  <div
-    className='bg-lake rounded-md mx-auto my-8 px-4 py-10 flex flex-row items-center'
-    style={{ maxWidth: '1000px' }}>
-    <div className='w-1/3 font-medium text-3xl'>{name}</div>
-    <div className='w-2/3 text-left px-10'>{description}</div>
-  </div>
-)
+import { FunctionComponent, PropsWithChildren, useState } from 'react'
+
+type TrackCardProps = {
+  name: string
+  description: string
+}
+
+const TrackCard: FunctionComponent<TrackCardProps> = (
+  props: PropsWithChildren<TrackCardProps>
+): JSX.Element => {
+  const [visible, setVisible] = useState(false)
+  return (
+    <>
+      <button
+        className='bg-lake mx-auto mt-8 px-4 py-8 flex flex-row items-center cursor-pointer transform transition-all duration-200 hover:scale-110 active:scale-100 active:outline-none outline-none'
+        style={{
+          borderTopLeftRadius: '30px',
+          borderBottomRightRadius: '30px',
+          maxWidth: '1000px',
+        }}
+        onClick={() => setVisible(!visible)}>
+        <div className='w-1/3 font-medium text-3xl'>{props.name}</div>
+        <div className='w-2/3 text-left px-10'>{props.description}</div>
+      </button>
+      {visible && (
+        <div style={{ maxWidth: '960px' }} className='my-0 py-6 mx-auto bg-cerulean rounded-b-md'>
+          {props.children}
+        </div>
+      )}
+    </>
+  )
+}
 
 export default TrackCard
