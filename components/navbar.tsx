d38089f105e25f2f45ec79e5c4375c866a9beb89
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-10-04 01:42:58 -0500

    Add "scroll to" functionality in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,39 @@
-const Navbar = () => (
+import { FunctionComponent, Ref } from 'react'
+
+const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop - 100)
+
+type NavbarProps = {
+  homeRef: Ref<unknown>
+  experienceRef: Ref<unknown>
+  partnersRef: Ref<unknown>
+  socialMediaRef: Ref<unknown>
+}
+
+const Navbar: FunctionComponent<NavbarProps> = (props: NavbarProps) => (
   <div
     id='navbar'
     className='py-6 w-full flex items-center justify-center fixed bg-ocean bg-opacity-75 top-0 z-50'>
-    <div className='min-w-1/2 flex items-center justify-between px-8'>
-      <a className='link font-medium text-xl text-white'>Home</a>
-      <a className='link font-medium text-xl text-grayed'>Experience</a>
-      <a className='link font-medium text-xl text-grayed'>Partners</a>
-      <a className='link font-medium text-xl text-grayed'>Social Media</a>
+    <div className='w-full sm:w-full md:w-full lg:w-2/3 xl:w-3/10 flex items-center justify-between px-8'>
+      <a
+        onClick={() => scrollToRef(props.homeRef)}
+        className='link cursor-pointer font-medium text-xl text-grayed'>
+        Home
+      </a>
+      <a
+        onClick={() => scrollToRef(props.experienceRef)}
+        className='link cursor-pointer font-medium text-xl text-grayed'>
+        Experience
+      </a>
+      <a
+        onClick={() => scrollToRef(props.partnersRef)}
+        className='link cursor-pointer font-medium text-xl text-grayed'>
+        Partners
+      </a>
+      <a
+        onClick={() => scrollToRef(props.socialMediaRef)}
+        className='link cursor-pointer font-medium text-xl text-grayed'>
+        Social Media
+      </a>
     </div>
   </div>
 )
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,20 +6,40 @@ import Tracks from '../components/tracks'
 import Partners from '../components/partners'
 import SocialMedia from '../components/social-media'
 import Footer from '../components/footer'
+import { useRef } from 'react'
 
 const Home = (): JSX.Element => {
+  const homeRef = useRef(null)
+  const experienceRef = useRef(null)
+  const partnersRef = useRef(null)
+  const socialMediaRef = useRef(null)
   return (
     <>
       <Head>
         <title>ACM Hacktoberfest</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Navbar />
-      <Hero />
-      <Speakers />
-      <Tracks />
-      <Partners />
-      <SocialMedia />
+      <Navbar
+        homeRef={homeRef}
+        experienceRef={experienceRef}
+        partnersRef={partnersRef}
+        socialMediaRef={socialMediaRef}
+      />
+      <section ref={homeRef}>
+        <Hero />
+      </section>
+      <section>
+        <Speakers />
+      </section>
+      <section ref={experienceRef}>
+        <Tracks />
+      </section>
+      <section ref={partnersRef}>
+        <Partners />
+      </section>
+      <section ref={socialMediaRef}>
+        <SocialMedia />
+      </section>
       <Footer />
     </>
   )
