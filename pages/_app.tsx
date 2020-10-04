270ed338faaa6c4d68c060df09f3d4797fef740c
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-10-04 02:09:36 -0500

    Adjust purge settings, remove obsolete purge methods

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app'
-import '../styles/tailwind.css'
 import '../styles/globals.css'
+import '../styles/tailwind.css'
 
 const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
 
--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,15 @@
-const purgecss = [
-  '@fullhuman/postcss-purgecss',
-  {
-    content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-]
-
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    process.env.NODE_ENV === 'production' ? purgecss : undefined,
-    'postcss-preset-env',
-  ],
+  plugins: {
+    tailwindcss: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
+  },
 }
