4dcd9945134bb6b98a2d2a2965ae9af949bda8cb
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-10-07 19:15:29 -0500

    Add FAQ link and component to page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
+import { useRef } from 'react'
 import Head from 'next/head'
 import Navbar from '../components/navbar'
 import Hero from '../components/hero'
 import Speakers from '../components/speakers'
 import Tracks from '../components/tracks'
 import Partners from '../components/partners'
+import FAQ from '../components/faq'
 import SocialMedia from '../components/social-media'
 import Footer from '../components/footer'
-import { useRef } from 'react'
 
 const Home = (): JSX.Element => {
   const homeRef = useRef(null)
   const experienceRef = useRef(null)
   const partnersRef = useRef(null)
+  const faqRef = useRef(null)
   const socialMediaRef = useRef(null)
   return (
     <>
@@ -23,6 +25,7 @@ const Home = (): JSX.Element => {
         homeRef={homeRef}
         experienceRef={experienceRef}
         partnersRef={partnersRef}
+        faqRef={faqRef}
         socialMediaRef={socialMediaRef}
       />
       <section ref={homeRef}>
@@ -37,6 +40,9 @@ const Home = (): JSX.Element => {
       {/* <section ref={partnersRef}>
         <Partners />
       </section> */}
+      <section ref={faqRef}>
+        <FAQ />
+      </section>
       <section ref={socialMediaRef}>
         <SocialMedia />
       </section>
