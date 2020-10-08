6c0c2584ba6b56e24b0d6eb91a9ced4999f19c55
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-10-07 19:14:46 -0500

    Add FAQ data

---
+++ faq.ts
@@ -0,0 +1,59 @@
+interface FAQFormat {
+  q: string
+  a: string
+}
+
+const faq: FAQFormat[] = [
+  {
+    q: 'What is ACM Hacktoberfest?',
+    a:
+      'ACM Hacktoberfest will be a full day event filled with tons of fun workshops, talks & fun events!',
+  },
+  {
+    q: 'When is ACM Hacktoberfest?',
+    a:
+      'ACM Hacktoberfest will take place on October 17th from 9am-9pm. There will also be lots of opportunities to socialize and have fun after the festivities!',
+  },
+  {
+    q: 'Where will ACM Hacktoberfest take place?',
+    a:
+      'ACM Hacktoberfest will be hosted on the ACM Discord (acmutd.co/discord) with several workshops & talks happening on Zoom.',
+  },
+  {
+    q: 'Do I need to stay for the full duration of the event?',
+    a:
+      'Nope! You’re welcome to participate by joining in as many or as few events as you would like! We would love to see you at as many of our events as possible!',
+  },
+  {
+    q: 'Can I win prizes?',
+    a:
+      'Yes! If you are a student at The University of Texas at Dallas & participate in attending workshops and talks, you will be eligible to win some awesome prizes.',
+  },
+  {
+    q: 'Will there be swag?',
+    a:
+      'As part of Hacktoberfest, Digital Ocean will be providing students who complete 4 open source pull requests (which you can do through our workshops) with a limited edition 2020 T-Shirt! Make sure to sign up at hacktoberfest.digitalocean.com!',
+  },
+  {
+    q: 'Where can I find a list of speakers & schedule for the event?',
+    a:
+      'Keep checking our website for more information! In addition to this we will also be sending out emails & making announcements on Discord about the same! If you don’t see our emails make sure to check your spam & add us to your contacts.',
+  },
+  {
+    q: 'Will there be opportunities to network and meet with other organizations?',
+    a:
+      'Yes! We have several partner organizations hosting events, panels, discussions and more! Check them out at the ECS Student Organization Fair happening at ACM Hacktoberfest & learn more about them viewing their information in the partners section!',
+  },
+  {
+    q: 'Where can I learn more about ACM?',
+    a:
+      'ACM has several divisions that offer lots of opportunities to students, check out our website at https://www.acmutd.co for the latest information!',
+  },
+  {
+    q: 'I have a question not answered here?',
+    a:
+      'Reach out to us by asking on the ACM Discord or shooting over an email to hacktoberfest@acmutd.co!',
+  },
+]
+
+export default faq
