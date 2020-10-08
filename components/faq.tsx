f31b856d2d5d5f1a7decd83e02ed8dcbc47a9faa
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-10-08 17:10:29 -0500

    Remove sparkles from FAQ heading to fix mobile issue
    FAQ heading is too long

--- components/faq.tsx
+++ components/faq.tsx
@@ -5,11 +5,8 @@ const FAQ = (): JSX.Element => {
   return (
     <div className='my-20'>
       <h2 className='font-bold text-4xl flex items-center justify-center'>
-      <img src='/img/leaf-1.svg' className='-ml-64 mt-2 h-12 absolute z-10' alt='' />
-      <img src='/img/sparkles.svg' className='ml-56 -mt-5 h-3 absolute z-10' alt='' />
-      <img src='/img/leaf-2.svg' className='ml-64 mt-3 h-6 absolute z-10' alt='' />
-      <span className='z-0'>Frequently Asked Questions</span>
-    </h2>
+        Frequently Asked Questions
+      </h2>
       <div className='my-12'>
         {faq.map(qa => (
           <FAQCard question={qa.q} answer={qa.a} key={JSON.stringify(qa)} />
