16db0c2c0e95f387c3f2514f3ee5f21408fd29c4
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-10-10 18:50:31 -0500

    Create leaderboard page

---
+++ pages/leaderboard.tsx
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import { LeaderboardNavbar } from '../components/navbar'
+import Rankings from '../components/rankings'
+
+const Leaderboard = (): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>ACM Hacktoberfest</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <LeaderboardNavbar />
+      <div className='mt-24'></div>
+      <h2 className='font-bold text-4xl flex items-center justify-center'>
+        <img src='/img/sparkles.svg' className='-ml-20 pr-8 -mt-6 h-3 absolute z-10' alt='' />
+        <img src='/img/leaf-2.svg' className='ml-32 pr-8 mt-3 h-6 absolute z-10' alt='' />
+        <span className='z-0'>Leaderboard</span>
+      </h2>
+      <div className='mt-8'>
+        <Rankings />
+      </div>
+    </>
+  )
+}
+
+export default Leaderboard
