903bd33e590fc207ddb34322f450b893438ffaad
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-10-17 00:53:37 -0500

    Add join links for every workshop

--- schedule-data.ts
+++ schedule-data.ts
@@ -21,6 +21,7 @@ const schedule: Event[] = [
     timeStr: '9:00–10:00 AM CST',
     id: 'checkin',
     joinLink: 'https://acmutd.co/discord',
+    description: "This isn't a real event, it's a reminder for you to wake up :)",
   },
   {
     track: 3,
@@ -54,6 +55,9 @@ const schedule: Event[] = [
     wide: true,
     timeStr: '10:00–10:30 AM CST',
     id: 'kickoff',
+    description:
+      'Kick off an amazing day of talks and workshops by hearing from the ACM Hacktoberfest organizer team and from Rocio Montes!',
+    joinLink: 'https://hack.acmutd.co/kickoff',
   },
   {
     track: 1,
@@ -63,6 +67,7 @@ const schedule: Event[] = [
     partner: 'Rocio Montes',
     timeStr: '10:30–11 AM CST',
     id: 'q-a-rocio-montes',
+    joinLink: 'https://hack.acmutd.co/rocio',
   },
   {
     track: 3,
@@ -74,6 +79,7 @@ const schedule: Event[] = [
     id: 'minecraft',
     description:
       'Come join Neal on the ACM Minecraft server for a bit to de-stress and hang out with other participants!',
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 1,
@@ -84,6 +90,7 @@ const schedule: Event[] = [
     id: 'women-tech-panel',
     description:
       'This event has six women in tech speak about their experiences and share advice to on how to be successful in a male-dominated field. Everyone is welcome to attend this useful event and get to know more about some wonderful ladies who are killing it in this field!',
+    joinLink: 'https://hack.acmutd.co/empower',
   },
   {
     track: 3,
@@ -95,6 +102,7 @@ const schedule: Event[] = [
     id: 'github-kahoot',
     description:
       'Think you know everything there is to know about GitHub? Test out your knowledge by competing with others on Kahoot! (All the relevant information to win this Kahoot will be present in one of our packets.)',
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 1,
@@ -106,6 +114,7 @@ const schedule: Event[] = [
     id: 'personal-site-workshop',
     description:
       'Learn how to make your own personal website in this amazing workshop hosted by ACM Education! After this event, you’ll take home a customizable portfolio website that showcases all your projects - and if you already have a personal website, learn how to integrate dark mode! No prior skills or knowledge required, and this is definitely something to add to your resume or GitHub! No prior skill or knowledge required, there’ll be people to help you every step of the way.',
+    joinLink: 'https://hack.acmutd.co/education',
   },
   {
     track: 2,
@@ -117,6 +126,7 @@ const schedule: Event[] = [
     id: 'ai-is-all-you-need',
     description:
       'Have a coffee chat with Willie Chalmers III, an AIS speaker who is prepared to convince everyone that AI is awesome and (probably) everything you need. Join him to learn more about what AI is and what the hype is about.',
+    joinLink: 'https://hack.acmutd.co/ais',
   },
   {
     track: 1,
@@ -128,6 +138,7 @@ const schedule: Event[] = [
     id: 'lunch-break-org-fair',
     description:
       'Looking for CS-related organizations to join this semester? Attend this organization fair to find out more information about the wonderful groups here at UTD. The organizations attending this event include ACM, SWE (Society of Women Engineers), CSG (Computer Security Group), and DSC (Developer Student Club).',
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 1,
@@ -139,6 +150,7 @@ const schedule: Event[] = [
     id: 'react-workshop',
     description:
       "In this workshop hosted by ACM Projects, you’ll get hands-on experience working with ReactJS while contributing to open source software by creating your very own virtual business cards! No prior skills or knowledge required, this could be an amazing thing to add to your resume or to a website so that it can be shown to friends and family. You'll kill two birds with one stone: you learn ReactJS, and get your own virtual business card!",
+    joinLink: 'https://hack.acmutd.co/projects',
   },
   {
     track: 2,
@@ -150,6 +162,7 @@ const schedule: Event[] = [
     id: 'wwc-spills-tea',
     description:
       'Feeling a bit down mentally? Join Sahana Belatur and Olivia Castro for a talk on Mental Health and Imposter Syndrome! With the current pandemic, there’s more need to discuss these topics and learn how to cope with them. Imposter syndrome strikes everyone, and is almost impossible to get rid of, so be sure to make it to this event to learn new techniques to gain more confidence in your abilities.',
+    joinLink: 'https://hack.acmutd.co/wwc',
   },
   {
     track: 3,
@@ -161,6 +174,7 @@ const schedule: Event[] = [
     id: 'skribbl-io-ostem',
     description:
       'Play Skribbl.io with other participants in this community event hosted by oSTEM! This is a great opportunity to relax, meet new people, and have fun! Show off your art skills and guessing techniques in this fun event hosted by Alex V.',
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 1,
@@ -171,6 +185,7 @@ const schedule: Event[] = [
     id: 'alum-panel-1',
     description:
       'Join ACM alums Krithika Suresh, Kelley Hoang, Reena Suh, Saman Attar, Izu Elechi, and Matthew Bierman and take home some great advice from some very successful people! For those looking to build connections, this event is for you.',
+    joinLink: 'https://hack.acmutd.co/alumni1',
   },
   {
     track: 3,
@@ -182,6 +197,7 @@ const schedule: Event[] = [
     id: 'swe-jeopardy',
     description:
       'Join SWE (Society of Women Engineers) for a game of Jeopardy to learn more about engineering, female STEM leaders, and SWE! Everyone is welcome to participate and learn more about this major organization at UTD! Attend to show off your skills, or de-stress with fellow participants while learning new things.',
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 1,
@@ -193,6 +209,7 @@ const schedule: Event[] = [
     id: 'data-analysis-workshop',
     description:
       'Learn how to analyse and visualize a stock’s data and metrics in this Python data analysis workshop hosted by ACM Research at 3 - 4 pm CST! You’ll be able to determine at a high level if a stock is overrated, so don’t miss it! ',
+    joinLink: 'https://hack.acmutd.co/research',
   },
   {
     track: 2,
@@ -204,6 +221,7 @@ const schedule: Event[] = [
     id: 'model-minority',
     description:
       'Learn more about the model minority myth in this talk hosted by Long Le from SASE.',
+    joinLink: 'https://hack.acmutd.co/sase',
   },
   {
     track: 3,
@@ -214,6 +232,7 @@ const schedule: Event[] = [
     timeStr: '3:30–4:00 PM CST',
     id: 'virtual-mystery-1',
     description: "Play a cooperative, virtual murder mystery with ACM Community's Hamzah!",
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 1,
@@ -225,6 +244,7 @@ const schedule: Event[] = [
     id: 'hackathons-tips-tricks',
     description:
       'Don’t know what you’re supposed to do in hackathons? Don’t worry, after this event you’ll be a pro at them! Join us as we discuss advice and strategies to get the most out of hackathons, especially ones in the current virtual setting!',
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 3,
@@ -236,6 +256,7 @@ const schedule: Event[] = [
     id: 'big-willie-cooks',
     description:
       '6 Items. 60 minutes. A race against the clock. Join Willie Chalmers III as he takes meal prep to the next level.',
+    joinLink: 'https://hack.acmutd.co/cooking',
   },
   {
     track: 2,
@@ -247,6 +268,7 @@ const schedule: Event[] = [
     id: 'gcp-workshop',
     description:
       'Join Sayan Guha to get an introduction to cloud computing and GCP (Google Cloud Platform). No prior skills or knowledge required, so there’s no reason not to attend and learn about this popular skill! Ask any questions you might have and get insight on how to improve your skills and knowledge about the industry!',
+    joinLink: 'https://hack.acmutd.co/dsc',
   },
   {
     track: 1,
@@ -258,6 +280,7 @@ const schedule: Event[] = [
     id: 'build-api-workshop',
     description:
       'Learn how to build an API in this workshop hosted by ACM Development! At the end of this workshop, you’ll be able to create your own sample API endpoint that accepts your email and returns your name and Discord tag. No prior skills required, so make sure to make it to this event hosted by Harsha Srikara & Eric Zhang! This is a great opportunity for beginners interested in APIs and backend development!',
+    joinLink: 'https://hack.acmutd.co/dev',
   },
   {
     track: 2,
@@ -269,6 +292,7 @@ const schedule: Event[] = [
     id: 'managing-college',
     description:
       'Managing your life in college can be hard, especially in the first years. For most people, transitioning from high school to college is going to a new place with no friends and it’s filled with lots of work. It’s not an easy process for most, so attend this informative event to learn more about how to be a successful college student! Ask any questions you might have and receive useful insight from someone successful who’s been in your shoes before.',
+    joinLink: 'https://hack.acmutd.co/wicys',
   },
   {
     track: 3,
@@ -279,6 +303,7 @@ const schedule: Event[] = [
     id: 'quarantine-bingo',
     description:
       'Try your luck at ACM’s unique bingo game. Be the first to get a bingo and win a prize! Don’t miss out, this might be one of the easiest ways to win something and hang out with others!',
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 2,
@@ -290,6 +315,7 @@ const schedule: Event[] = [
     id: 'wtf-defi',
     description:
       "WTF is DeFi? Blockchain Club's got you covered. Learn more about this emerging field of finance, backed by the crypto revolution.",
+    joinLink: 'https://hack.acmutd.co/blockchain',
   },
   {
     track: 3,
@@ -301,6 +327,7 @@ const schedule: Event[] = [
     id: 'bob-ross-paint',
     description:
       'If you’re an artist or someone who loves watching people make art, make sure to attend this painting event where everyone paints on Microsoft Paint! Share your art, watch a relaxing video, meet other artists and possibly gain new friends! A must-attend event for Bob Ross lovers and aspiring artists!',
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 1,
@@ -312,6 +339,7 @@ const schedule: Event[] = [
     id: 'comp-graphics-workshop',
     description:
       'Attend this Computer Graphics workshop to learn how to use WebGL, a powerful Javascript API for high-performance graphics. You’ll learn how to make your own fancy 3D shapes and visuals! No prior skills or knowledge required.',
+    joinLink: 'https://hack.acmutd.co/community',
   },
   {
     track: 2,
@@ -323,6 +351,7 @@ const schedule: Event[] = [
     id: 'bridging-the-gap-software-hardware',
     description:
       'Do you like both hardware and software and are finding it hard to choose just one? Join Saravanan Narayanan, as he discusses bridging the gap between software and hardware.',
+    joinLink: 'https://hack.acmutd.co/ieee',
   },
   {
     track: 2,
@@ -333,6 +362,7 @@ const schedule: Event[] = [
     timeStr: '7:30–7:45 PM CST',
     id: 'accessibility-basics',
     description: '',
+    joinLink: 'https://hack.acmutd.co/gwc',
   },
   {
     track: 1,
@@ -343,6 +373,7 @@ const schedule: Event[] = [
     id: 'mlh-fellowship-talk',
     description:
       'Join Anirudh Emmadi as he speaks about his experience contributing to open source through the MLH Fellowship. Attend this short event to gain valuable information about an amazing program!',
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 3,
@@ -351,15 +382,19 @@ const schedule: Event[] = [
     name: 'Virtual Murder Mystery #2',
     timeStr: '7:30–8:00 PM CST',
     id: 'virtual-mystery-2',
+    description: "Play a cooperative, virtual murder mystery with ACM Community's Hamzah!",
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 3,
     offset: 44,
     length: 30,
-    name: 'Trivia with SkillsUSA',
+    name: 'Soft Skills Trivia with SkillsUSA',
     partner: 'SkillsUSA',
     timeStr: '8:00–8:30 PM CST',
     id: 'skillsusa-trivia',
+    description: 'Play a soft skills trivia game with SkillsUSA!',
+    joinLink: 'https://acmutd.co/discord',
   },
   {
     track: 1,
@@ -370,6 +405,7 @@ const schedule: Event[] = [
     id: 'alum-panel-2',
     description:
       'Join ACM alums Matthew Bierman, Aneesh Saripalli, and Bharat Arimilli to learn about their experiences in ACM and take home some great advice from successful people! For those looking to build connections, this event is for you.',
+    joinLink: 'https://hack.acmutd.co/alumni2',
   },
   {
     track: 1,
@@ -380,6 +416,7 @@ const schedule: Event[] = [
     timeStr: '8:45–9:00 PM CST',
     id: 'wrap-up',
     description: 'Wrap up an amazing day of talks and workshops with us!',
+    joinLink: 'https://hack.acmutd.co/closing',
   },
   {
     track: 3,
@@ -390,6 +427,7 @@ const schedule: Event[] = [
     timeStr: '9:00 PM CST onwards',
     id: 'after-party',
     description: 'End the day by playing Among Us with the ACM Community team!',
+    joinLink: 'https://acmutd.co/discord',
   },
 ]
 
